40b9b6a357860da0dc29329fc3e852f29d2e8d13
Нестеров <anime.cula@mail.ru>
2022-10-15 11:42:55 +0700

    sites

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -52,3 +52,16 @@ class BookInstanceAdmin(admin.ModelAdmin):
             'fields': ('status', 'due_back')
         }),
     )
+
+    class BookInstanceAdmin(admin.ModelAdmin):
+        list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+        list_filter = ('status', 'due_back')
+
+        fieldsets = (
+            (None, {
+                'fields': ('book', 'imprint', 'id')
+            }),
+            ('Availability', {
+                'fields': ('status', 'due_back', 'borrower')
+            }),
+        )
